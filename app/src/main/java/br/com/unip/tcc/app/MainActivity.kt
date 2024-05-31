2f7ef5beb9cc1d06236a2b126b9ddc1e55d1657d
maurilio-oliver <maurilio1.5@hotmail.com>
2024-05-31 18:49:41 -0300

    initial commit

--- app/src/main/java/br/com/unip/tcc/app/MainActivity.kt
+++ app/src/main/java/br/com/unip/tcc/app/MainActivity.kt
@@ -1,11 +1,45 @@
 package br.com.unip.tcc.app
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        val intent = Intent(this, InfoProdutoActivity::class.java)
+
+        startActivity(intent)
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.menu_login) {
+            startActivity(Intent(this, LoginActivity::class.java))
+            return true
+        } else if (id == R.id.menu_cadastro) {
+            startActivity(Intent(this, CadastroActivity::class.java))
+            return true
+        } else if (id == R.id.menu_perfil) {
+            //startActivity(Intent(this, ::class.java))
+            return true
+        } else if (id == R.id.menu_sobre) {
+            //startActivity(Intent(this, ::class.java))
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
